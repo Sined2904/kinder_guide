0883b10002b013f045c714262f75d9fa58c77ed0
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-09-27 22:03:08 +0300

    comment model, admin

--- kinder_guide/comments/admin.py
+++ kinder_guide/comments/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register
+class Comment(admin.ModelAdmin):
+    list_display = (
+        'comment_post',
+        'author',
+        'content',
+        'date_posted',
+        'parent'
+    )
+    list_filter = (
+        'parent',
+        'date_posted'
+    )
+    search_fields = (
+        'author',
+        'content',
+        'date_posted'
+    )
