fb7bb69f708443614b3224b50df4b4d467c6a735
Соловьев Эдуард Павлович <soloviev@ferex.td>
2023-10-03 15:18:27 +0300

    models_serializers_admin_urls_settings created

--- kinder_guide/kinder_guide/settings.py
+++ kinder_guide/kinder_guide/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'user.apps.UserConfig',
     'kinder_guide',
     'comments',
-    'user',
     'about_us',
 ]
 
@@ -121,9 +124,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.MyUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'HIDE_USERS': False,
+    'SERIALIZERS': {
+        'user': 'user.serializers.CustomUserSerializer',
+        'user_list': 'user.serializers.CustomUserSerializer',
+        'current_user': 'user.serializers.CustomUserSerializer',
+        'user_create': 'user.serializers.UserCreateSerializer',
+    }
+}
--- kinder_guide/user/models.py
+++ kinder_guide/user/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class MyUser(AbstractUser):
+    """Кастомная модель юзера."""
+
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        validators=[
+            RegexValidator(regex=r'^[\w.@+-]',
+                           message='Недопустимые символы в имени пользователя')
+        ]
+    )
+    password = models.CharField(
+        'Пароль',
+        max_length=150,
+    )
+    email = models.EmailField(
+        'e-mail',
+        max_length=254,
+        unique=True,
+    )
+    first_name = models.TextField('Имя', max_length=150,)
+    last_name = models.TextField('Фамилия', max_length=150,)
+    phone = models.CharField(
+        'Номер телефона',
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ['-date_joined', ]
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
