4d07cc32f9db3d886bee3c1f85d60d9e60b983e4
Денис Пиневич <119804808+Sined2904@users.noreply.github.com>
2023-10-05 21:10:27 +0300

    Update settings.py

--- kinder_guide/kinder_guide/settings.py
+++ kinder_guide/kinder_guide/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-yir#vh&^tzfltm4qd=1*wp6@4()41%n0*mz2edk0b7g6f(q_kb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kinder.acceleratorpracticum.ru']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kinder.acceleratorpracticum.ru', '45.86.181.122']
 
 
 # Application definition
