36b1154fd8b01ea471416e65f248016fe402e6c1
Kristina Sashkina <kristinasashkina@yandex.ru>
2023-10-08 12:50:09 +0300

    Добавление env

--- kinder_guide/kinder_guide/settings.py
+++ kinder_guide/kinder_guide/settings.py
@@ -66,24 +66,24 @@ WSGI_APPLICATION = 'kinder_guide.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'kinder_guide'),
-#         'USER': os.getenv('POSTGRES_USER', 'kinder_guide_user'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', ''),
-#         'PORT': os.getenv('DB_PORT', 5432)
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'kinder_guide'),
+        'USER': os.getenv('POSTGRES_USER', 'kinder_guide_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
