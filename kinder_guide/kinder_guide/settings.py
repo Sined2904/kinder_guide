8a9f3dd01ea65efeef89eae535b63c27024510f9
Денис Пиневич <Den2904@yandex.ru>
2023-10-12 20:20:25 +0300

    Добавил ограничение запросов

--- kinder_guide/kinder_guide/settings.py
+++ kinder_guide/kinder_guide/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-yir#vh&^tzfltm4qd=1*wp6@4()41%n0*mz2edk0b7g6f(q_kb
 DEBUG = False
 
 # IF TRUE - USES SQLITE3 FOR LOCAL TASTING, IF FALSE - USES POSTGRESQL
-LOCAL_DB = True
+LOCAL_DB = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kinder.acceleratorpracticum.ru', '45.86.181.122']
 
