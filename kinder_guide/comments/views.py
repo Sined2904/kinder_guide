6c6f885133d8416140ac7c32d80f0a0ab3e19c63
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-10-17 01:17:22 +0300

    views

--- kinder_guide/comments/views.py
+++ kinder_guide/comments/views.py
@@ -1,41 +1,11 @@
-from django.shortcuts import redirect
-from django.views.generic import DetailView
+from requests import Response
+from rest_framework import status, viewsets
 
-from ..education.models import Education
-from .models import Review
-from .forms import ReviewForm
+from .constants import UNAUTHORIZED
+from .models import ReviewCourse, ReviewKindergarten, ReviewSchool
+from .serializers import (ReviewCourseSerializer, ReviewKindergartenSerializer,
+                          ReviewSchoolSerializer)
 
 
-class PageDetailView(DetailView):
-    template_name = 'sample_page.html'
-    model = Education
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-        connected_reviews = Review.objects.filter(
-            ReviewPost=self.get_object()
-        )
-        number_of_reviews = connected_reviews.count()
-        data['reviews'] = connected_reviews
-        data['number_of_review'] = number_of_reviews
-        data['review_form'] = ReviewForm()
-        return data
-
-    def post(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            review_form = ReviewForm(request.POST)
-            if review_form.is_valid():
-                content = review_form.cleaned_data['content']
-                try:
-                    parent = review_form.cleaned_data['parent']
-                except KeyError:
-                    parent = None
-
-            new_review = Review(
-                content=content,
-                author=request.user,
-                ReviewPost=self.get_object(),
-                parent=parent,
-            )
-            new_review.save()
-            return redirect(request.path_info)
+class ReviewSchoolViewSet(viewsets.ModelViewSet):
+    pass
