59d13b958e7c66d606ae57d9f679679c92bc5593
Денис Пиневич <Den2904@yandex.ru>
2023-10-17 19:50:29 +0300

    update urls

--- kinder_guide/api/urls.py
+++ kinder_guide/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from django.contrib import admin
 
 from .views import SchoolViewSet, KindergartensViewSet, CourseViewSet
-
+from news.feeds import LatestNewsFeed
 
 app_name = 'api'
 
@@ -14,4 +15,7 @@ router.register('courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('feed/', LatestNewsFeed(), name = 'news_feed'),
 ]
