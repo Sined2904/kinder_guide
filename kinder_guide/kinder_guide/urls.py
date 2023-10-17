1662783d675d1c36dd03be573254efc37bc3e43d
Денис Пиневич <Den2904@yandex.ru>
2023-10-17 20:42:52 +0300

    update url

--- kinder_guide/kinder_guide/urls.py
+++ kinder_guide/kinder_guide/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1/', include('api.urls', namespace='api')),
+    path('api/', include('api.urls', namespace='api')),
     path('admin/', admin.site.urls),
     path(
         'redoc/',
