90a610514ce5288b5e50cb016bf2dcb4e6b10a9c
Денис Пиневич <Den2904@yandex.ru>
2023-10-17 20:24:40 +0300

    update url

--- kinder_guide/kinder_guide/urls.py
+++ kinder_guide/kinder_guide/urls.py
@@ -1,11 +1,11 @@
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 from django.urls import path, include
-from news.feeds import LatestNewsFeed
 
 urlpatterns = [
     path('api/v1/', include('api.urls', namespace='api')),
-    path('feed/', LatestNewsFeed(), name = 'news_feed'),
+    path('admin/', admin.site.urls),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
