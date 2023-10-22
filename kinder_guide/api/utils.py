01fc603126a80eb549fa6efbfe8c27cf92e7b317
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-10-22 19:37:00 +0300

    avg rating NoneType fix

--- kinder_guide/api/utils.py
+++ kinder_guide/api/utils.py
@@ -3,4 +3,6 @@ from django.db.models import Avg
 
 def get_avg_rating(model, obj):
     rating = model.objects.filter(review_post=obj.id).aggregate(Avg('rating'))
+    if not rating['rating__avg']:
+        return rating['rating__avg']
     return round(rating['rating__avg'], 1)
